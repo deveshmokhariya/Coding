283b5f6257930587683832e4d5883dfe98a457c1
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-07-20 23:49:33 +0530

    Time: 1 ms (100%), Space: 48.7 MB (87.52%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,21 +1,40 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        int ans[] = new int[n1 + n2];
-        for(int i =0; i<n1; i++){
-        ans[i] = nums1[i];
+        int n=nums1.length;
+        int m=nums2.length;
+        if(n>m){
+            return findMedianSortedArrays(nums2,nums1);
         }
-        for(int i =0; i<n2; i++){
-        ans[n1 + i] = nums2[i];
-        }
-        Arrays.sort(ans);
-        int n = ans.length;
+        int size=(n+m);
+        int total=(n+m+1)/2;
+        int l=0,r=n;
+        while(l<=r){
+            int takeX=(l+r)/2;
+            int takeY=total-takeX;
 
-        int mid = n / 2;
-        if(n%2!=0){
-            return ans[mid];
+            int partitionXLeft=takeX==0?Integer.MIN_VALUE:nums1[takeX-1];
+            int partitionXRight=takeX==n?Integer.MAX_VALUE:nums1[takeX];
+
+            int partitionYLeft=takeY==0?Integer.MIN_VALUE:nums2[takeY-1];
+            int partitionYRight=takeY==m?Integer.MAX_VALUE:nums2[takeY];
+
+            if(partitionXLeft<=partitionYRight && partitionXRight>=partitionYLeft){
+                if(size%2==0){
+                    int a=Math.max(partitionXLeft,partitionYLeft);
+                    int b=Math.min(partitionXRight,partitionYRight);
+                    return (a+b)/2.0;
+                }
+                else{
+                    return Math.max(partitionXLeft,partitionYLeft);
+                }
+            }
+            else  if(partitionXLeft>partitionYRight){
+                r=takeX-1;
+            }
+            else{
+                l=takeX+1;
+            }
         }
-        return ((long) ans[mid - 1] + ans[mid]) / 2.0;
+        return 0.0;
     }
 }
