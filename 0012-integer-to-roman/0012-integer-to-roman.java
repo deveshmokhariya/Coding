97af484e853f9f16828981269e8ae8603378bb90
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-08-03 14:42:38 +0530

    Time: 3 ms (99.83%), Space: 46.4 MB (61.42%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.java
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -1,30 +1,18 @@
 class Solution {
     public String intToRoman(int num) {
-        HashMap<Integer, String> map = new HashMap<>();
-        map.put(1000, "M");
-        map.put(900, "CM");
-        map.put(500, "D");
-        map.put(400, "CD");
-        map.put(100, "C");
-        map.put(90, "XC");
-        map.put(50, "L");
-        map.put(40, "XL");
-        map.put(10, "X");
-        map.put(9, "IX");
-        map.put(5, "V");
-        map.put(4, "IV");
-        map.put(1, "I");
-        int[] values = {
-            1000, 900, 500, 400,
-            100, 90, 50, 40,
-            10, 9, 5, 4,
-            1
+        int []val={ 1000,900,500,400,
+                    100,90,50,40,
+                    10,9,5,4,1
         };
-        StringBuilder ans = new StringBuilder();
-        for (int value : values) {
-            while (num >= value) {
-                ans.append(map.get(value));
-                num -= value;
+        String[] roman={"M","CM","D","CD",
+                        "C","XC","L","XL",
+                        "X","IX","V","IV","I"
+        };
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<val.length;i++){
+            while(num>=val[i]){
+                ans.append(roman[i]);
+                num-=val[i];
             }
         }
         return ans.toString();
