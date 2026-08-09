b3a7474f28fbd307df9719910788a6b7a208407f
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-08-09 11:02:39 +0530

    Time: 2 ms (13.42%), Space: 42.9 MB (68.33%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,18 +1,78 @@
+// class Solution {
+//     public int strStr(String haystack, String needle) {
+//        int m = needle.length(); 
+//        int n = haystack.length(); 
+//        for(int i = 0; i<=n-m; i++){
+//         int j;
+//         for(j=0; j<m; j++){
+//             if(needle.charAt(j) != haystack.charAt(i+j)){
+//                 break;
+//             }
+//         }
+//         if(j==m){
+//             return i;
+//         }
+//        }
+//        return -1;
+//     }
+// }
 class Solution {
     public int strStr(String haystack, String needle) {
-       int m = needle.length(); 
-       int n = haystack.length(); 
-       for(int i = 0; i<=n-m; i++){
-        int j;
-        for(j=0; j<m; j++){
-            if(needle.charAt(j) != haystack.charAt(i+j)){
-                break;
+
+        int n = haystack.length();
+        int m = needle.length();
+
+        int[] lps = new int[m];
+
+        // Create LPS array
+        int len = 0;
+        int i = 1;
+
+        while (i < m) {
+
+            if (needle.charAt(i) == needle.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } 
+            else {
+
+                if (len == 0) {
+                    lps[i] = 0;
+                    i++;
+                } 
+                else {
+                    len = lps[len - 1];
+                }
             }
         }
-        if(j==m){
-            return i;
+
+        // KMP search
+        i = 0;
+        int j = 0;
+
+        while (i < n) {
+
+            if (haystack.charAt(i) == needle.charAt(j)) {
+                i++;
+                j++;
+
+                // Complete pattern found
+                if (j == m) {
+                    return i - j;
+                }
+            } 
+            else {
+
+                if (j == 0) {
+                    i++;
+                } 
+                else {
+                    j = lps[j - 1];
+                }
+            }
         }
-       }
-       return -1;
+
+        return -1;
     }
 }
