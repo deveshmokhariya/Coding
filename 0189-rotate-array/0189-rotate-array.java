5276777bbdee91fd17ea958f303edbbf88fb0dc7
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-08-13 15:07:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -19,3 +19,21 @@ class Solution {
         }
     }
 }
+// class Solution {
+//     public void rotate(int[] nums, int k) {
+//         int n = nums.length;
+//         k = k % n;
+//         rev(nums, 0, n-1);
+//         rev(nums, 0, k-1);
+//         rev(nums, k, n-1);
+//     }
+//     public void rev(int[] nums, int low, int high){
+//         while(low<high){
+//             int temp =  nums[low];
+//             nums[low] = nums[high];
+//             nums[high] = temp;
+//             low++;
+//             high--;
+//         }
+//     }
+// }
