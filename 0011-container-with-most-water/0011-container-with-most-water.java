79bacd30199c517816725c7d99d15dae8150b602
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-08-13 19:18:42 +0530

    Time: 6 ms (82.94%), Space: 77.3 MB (69.21%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxArea(int[] height) {
-
-
-        int i=0;
-        int j=height.length-1;
-        int maxArea=0;
+        int i = 0;
+        int j = height.length - 1;
+        int maxArea = 0;
         while(i<j){
-            int currArea=(j-i)*Math.min(height[i], height[j]);
-            maxArea =Math.max(maxArea,currArea);
-
+            int currArea = (j-i) * Math.min(height[i], height[j]);
+            maxArea = Math.max(maxArea, currArea);
             if(height[i]>height[j]){
                 j--;
             }
@@ -19,18 +16,7 @@ class Solution {
                 i++;
                 j--;
             }
-
         }
         return maxArea;
-        
     }
-    static {
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            try (FileWriter fw = new FileWriter("display_runtime.txt")) {
-                fw.write("0");
-            } catch (Exception e) {}
-        }));
-    }
-
 }
-    
