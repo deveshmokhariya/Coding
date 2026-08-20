ec181540e1c75c0faab88c826a0fa94192573c55
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-08-20 12:45:51 +0530

    Time: 0 ms (100%), Space: 47 MB (13.6%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -9,25 +9,28 @@
  *     }
  * }
  */
+
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-      ListNode slow = head;
-      ListNode fast = head;
-      while(fast != null && fast.next != null){
-        slow = slow.next;
-        fast = fast.next.next;
-        if(slow == fast){
-            slow = head;
-            int index = 0;
-            while(slow != fast){
-                slow = slow.next;
-                fast = fast.next;
-                index++;
+        {
+            ListNode slow,fast;
+            slow=fast=head;
+            while(fast!=null&&fast.next!=null)
+            {
+                slow=slow.next;
+                fast=fast.next.next;
+                if(slow==fast)
+                {
+                    ListNode entry=head;
+                    while(entry!=slow)
+                    {
+                        entry=entry.next;
+                        slow=slow.next;
+                    }
+                    return entry;
+                }
             }
-            System.out.println("tail connects to node index " + index);
-            return slow;
+            return null;
         }
-      }
-     return null;
     }
 }
