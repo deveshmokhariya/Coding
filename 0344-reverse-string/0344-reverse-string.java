e4ada030f121fc08ef6c1df6fd4d63566061a4d3
Devesh Mokhariya <159990354+deveshmokhariya@users.noreply.github.com>
2026-09-07 16:59:26 +0530

    Time: 5 ms (3.21%), Space: 50.6 MB (5.35%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.java
+++ 0344-reverse-string/0344-reverse-string.java
@@ -1,13 +1,32 @@
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int left = 0;
+//         int right = s.length - 1;
+//         while(left < right){
+//             char temp = s[left];
+//             s[left] = s[right];
+//             s[right] = temp;
+//             left++;
+//             right--;
+//         }
+//     }
+// }
+
+
+//Using Stack
 class Solution {
     public void reverseString(char[] s) {
-        int left = 0;
-        int right = s.length - 1;
-        while(left < right){
-            char temp = s[left];
-            s[left] = s[right];
-            s[right] = temp;
-            left++;
-            right--;
+        Stack<Character> str = new Stack<>();
+
+        for (int i = 0; i < s.length; i++) {
+            str.push(s[i]);
+        }
+
+        int i = 0;
+
+        while (!str.isEmpty()) {
+            s[i] = str.pop();
+            i++;
         }
     }
 }
